f28ecbb7bb4be90c5054aef0447608b1eeede57f
caio caminha <caminhacaiopro@gmail.com>
2021-01-23 07:55:28 -0300

    Evitando SQL Injection com PrepareStatement

--- src/TestaInsercao.java
+++ src/TestaInsercao.java
@@ -1,16 +1,24 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class TestaInsercao {
 	public static void main(String[] args) throws  SQLException{
+		String Nome = "";
+		String Descricao = "";
+		
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.RecuperarConexao();
 		
-		Statement stm = connection.createStatement();
-		stm.execute("INSERT INTO PRODUTO (nome, descricao)VALUES('NOTEBOOK', 'NOTEBOOK SAMSUNG')"
-					,Statement.RETURN_GENERATED_KEYS);
+		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao)VALUES(?, ?)"
+				,Statement.RETURN_GENERATED_KEYS);
+		stm.setString(1, Nome);
+		stm.setString(2, Descricao);
+		
+		stm.execute();
+		
 		ResultSet rst = stm.getGeneratedKeys();
 		while(rst.next()) {
 			Integer id = rst.getInt(1);
