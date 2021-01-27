886482f6660d72c05388b3c9c76d061ba2c96b53
caio caminha <caminhacaiopro@gmail.com>
2021-01-27 07:24:06 -0300

    Adicionando o Pool de Connexões

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -1,11 +1,29 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.Connection; 
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 //Fábrica de conexao
 public class ConnectionFactory {
+	//interface java que permite a comunicação entre a aplicação e os drivers de pool de conexão
+	public DataSource dataSource;
+	public ConnectionFactory() {
+		//classe do c3p0 que permite realizar as configurações jdbc
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("caminha123");
+		
+		comboPooledDataSource.setMaxPoolSize(15);
+		
+		this.dataSource = comboPooledDataSource;
+	}
+	
+	
+	
 	public Connection RecuperarConexao() throws SQLException {
-		return DriverManager
-				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "caminha123");
+		return this.dataSource.getConnection();
 	}
 }
